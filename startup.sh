813a9c48154c12c079d6f7fedb3d82e320980489
rakshanagendra <nagendraraksha@gmail.com>
2026-06-28 15:50:24 +0200

    Corrected hF username

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ from huggingface_hub import snapshot_download
 import os
 
 snapshot_download(
-    repo_id="rakshaanagendra/rag-index-data",
+    repo_id="rakshanagendra/rag-index-data",
     repo_type="dataset",
     local_dir="data/processed",
     token=os.environ.get("HF_TOKEN")
